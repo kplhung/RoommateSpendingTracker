92e55a7d8da03d25aba20add0b8af4325ee1f1b5
kplhung <khung61@gmail.com>
2018-03-07 14:11:34 -0500

    created new class for email notifications

---
+++ app/src/main/java/com/example/kallyruan/roommateexpense/EmailNotify.java
@@ -0,0 +1,13 @@
+package com.example.kallyruan.roommateexpense;
+
+/**
+ * Created by Kelly on 3/7/2018.
+ */
+
+public class EmailNotify {
+    // check database for all users and their bills. If a bill has a due date of TODAY,
+    // then send an email
+    void notifyUsers() {
+
+    }
+}
