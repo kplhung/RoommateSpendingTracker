5d1f611ca18b4b0b2c8fced5840b59deecf4fc6a
kallyrhy <34464675+kallyrhy@users.noreply.github.com>
2018-03-09 00:27:30 -0500

    add groupManage activity

---
+++ app/src/main/java/com/example/kallyruan/roommateexpense/GroupManageActivity.java
@@ -0,0 +1,17 @@
+package com.example.kallyruan.roommateexpense;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kallyruan on 9/3/18.
+ */
+
+public class GroupManageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill_list);
+    }
+}
