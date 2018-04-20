13336c5fba43f961b8252c8ca49f0aa2ec9e25a9
Yi Di Wu <lilyidi@gmail.com>
2018-04-20 00:21:55 -0400

    show list of group members payment for each bill

--- app/src/main/java/com/example/kallyruan/roommateexpense/BillPkg/Bill.java
+++ app/src/main/java/com/example/kallyruan/roommateexpense/BillPkg/Bill.java
@@ -1,5 +1,8 @@
 package com.example.kallyruan.roommateexpense.BillPkg;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * POJO that keeps track of individual Bill information
  * Created by Lily on 2/17/2018.
@@ -7,13 +10,18 @@ package com.example.kallyruan.roommateexpense.BillPkg;
 
 public class Bill {
     private String name, amount, dueDate, billID, desc;
+    private ArrayList<String []> other_payers;
 
     public Bill(String name, String amount, String dueDate, String billID, String desc) {
+        this(name, amount, dueDate, billID, desc, null);
+    }
+    public Bill(String name, String amount, String dueDate, String billID, String desc, ArrayList<String []> other_payers) {
         this.name = name;
         this.amount = amount;
         this.dueDate = dueDate;
         this.billID = billID;
         this.desc = desc;
+        this.other_payers = other_payers;
     }
 
     /**
@@ -45,5 +53,9 @@ public class Bill {
 
     public String getBillID() {
         return this.billID;
+    }
+
+    public ArrayList<String []> getOtherPayers() {
+        return other_payers;
     }
 }
--- app/src/main/java/com/example/kallyruan/roommateexpense/BillPkg/BillAdapter.java
+++ app/src/main/java/com/example/kallyruan/roommateexpense/BillPkg/BillAdapter.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.BaseExpandableListAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,13 +13,17 @@ import com.example.kallyruan.roommateexpense.UserPkg.LoginActivity;
 import com.example.kallyruan.roommateexpense.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Lily on 2/17/2018.
  */
 
-public class BillAdapter extends BaseAdapter {
+public class BillAdapter extends BaseExpandableListAdapter {
     private Activity mActivity;
+    private DBQueries instance;
     private ArrayList<Bill> bills;
     private TextView billName;
     private TextView billAmt;
@@ -32,39 +36,101 @@ public class BillAdapter extends BaseAdapter {
         this.bills = bills;
         this.groupID = groupID;
         this.userName = userName;
+        this.instance = DBQueries.getInstance();
     }
+
+//    /**
+//     * @return number of bills
+//     */
+//    @Override
+//    public int getCount() {
+//        return bills.size();
+//    }
+//
+//    @Override
+//    public Object getItem(int i) {
+//        return null;
+//    }
+//
+//    @Override
+//    public long getItemId(int i) {
+//        return 0;
+//    }
 
     /**
-     * @return number of bills
-     */
-    @Override
-    public int getCount() {
-        return bills.size();
-    }
-
-    @Override
-    public Object getItem(int i) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int i) {
-        return 0;
-    }
-
-    /**
-     * @param position index of group for which view is desired
+     *
+     * @param groupPosition
+     * @param childPosition
+     * @param isLastChild
      * @param view
-     * @param viewGroup
-     * @return View for a group
+     * @param parent
+     * @return
      */
     @Override
-    public View getView(int position, final View view, ViewGroup viewGroup) {
+    public View getChildView(int groupPosition, final int childPosition,
+                             boolean isLastChild, View view, ViewGroup parent) {
         LayoutInflater inflater = mActivity.getLayoutInflater();
         final View createdView;
 
         if(view == null){
-            createdView = inflater.inflate(R.layout.bills_list_row, null);
+            createdView = inflater.inflate(R.layout.bill_list_child, null);
+        } else {
+            createdView = view;
+        }
+
+        final Bill bill = bills.get(groupPosition);
+        TextView bill_payer = (TextView) createdView.findViewById(R.id.bill_list_payer_name);
+        TextView bill_payer_amt = (TextView) createdView.findViewById(R.id.bill_list_payer_amt);
+
+        String[] payer = bill.getOtherPayers().get(childPosition);
+        bill_payer.setText(payer[0]);
+        bill_payer_amt.setText(payer[1]);
+
+        return createdView;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return bills.size();
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return instance.getGroupMembers(instance.getGroupIdForBill(bills.get(i).getBillID())).size();
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return bills.get(i);
+    }
+
+    @Override
+    public Object getChild(int i, int i1) {
+        return instance.getGroupMembers(bills.get(i).getBillID()).get(i1);
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return i;
+    }
+
+    @Override
+    public long getChildId(int i, int i1) {
+        return i1;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int position, boolean b, View view, ViewGroup viewGroup) {
+        LayoutInflater inflater = mActivity.getLayoutInflater();
+        final View createdView;
+
+        if(view == null){
+            createdView = inflater.inflate(R.layout.bills_list_group, null);
             billName = (TextView) createdView.findViewById(R.id.bill_name);
             billAmt = (TextView) createdView.findViewById(R.id.bill_amount);
             billDate = (TextView) createdView.findViewById(R.id.bill_date);
@@ -88,5 +154,10 @@ public class BillAdapter extends BaseAdapter {
             }
         });
         return createdView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return false;
     }
 }
